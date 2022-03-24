e816a752224dcbfb167c8a596b06b7974e726ceb
pawanyogi <yogipawanofficial01@gmail.com>
2022-03-25 02:31:34 +0530

    Header added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <Navbar.Brand as={Link} to="/">
-            NatureFresh
+            MernShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
